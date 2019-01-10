d771a46a3a12c0bca09a389bd4ea6067307d286d
maxsIT <maxsit2000@gmail.com>
2019-01-10 02:01:38 +0200

    auth

---
+++ DraGLand/DraGLand/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DraGLand.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Users = "maxsit2000@gmail.com, glazkov.hanter11@gmail.com")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
