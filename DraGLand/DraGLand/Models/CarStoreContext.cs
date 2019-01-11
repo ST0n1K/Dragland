89c2f3f1a4c45286eb73e9ab080ba1f60236cdd4
maxsIT <maxsit2000@gmail.com>
2019-01-11 22:02:41 +0200

    store

---
+++ DraGLand/DraGLand/Models/CarStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DraGLand.Models
+{
+    public class CarStoreContext : DbContext
+    {
+        public DbSet<CarStore> CarStores { get; set; }
+    }
+}
